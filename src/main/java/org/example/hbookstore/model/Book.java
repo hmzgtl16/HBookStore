c3925bb9ec06a3825f1c55c76b229fdf34ef891d
hmzgt <hmzgtl16@gmail.com>
2025-06-24 23:45:12 +0100

    feature: add Book entity

---
+++ src/main/java/org/example/hbookstore/model/Book.java
@@ -0,0 +1,24 @@
+package org.example.hbookstore.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "books")
+public class Book {
+
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Long authorId; // FK to Author
+}
+
