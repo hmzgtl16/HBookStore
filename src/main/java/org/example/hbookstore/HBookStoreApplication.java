98a903254b9da9537d325533b1fc382adc8bb98a
hmzgt <hmzgtl16@gmail.com>
2025-06-24 23:08:36 +0100

    Initial commit

---
+++ src/main/java/org/example/hbookstore/HBookStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.hbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hbookstore/HBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
