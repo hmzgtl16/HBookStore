938527b6a2ab524a7a67d3e1b8b7ca85e6260b47
hmzgt <hmzgtl16@gmail.com>
2025-06-25 16:51:26 +0100

    add findByNationality and findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase in AuthorRepository

--- src/main/java/org/example/hbookstore/author/domain/AuthorRepository.java
+++ src/main/java/org/example/hbookstore/author/domain/AuthorRepository.java
@@ -1,7 +1,19 @@
 package org.example.hbookstore.author.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Long>, PagingAndSortingRepository<Author, Long> {
+
+    Page<Author> findByNationality(String nationality, Pageable pageable);
+
+    Page<Author> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            String firstName,
+            String lastName,
+            Pageable pageable
+    );
 }
