e668602be4743dc05805c35a232ca62d60d91c34
hmzgt <hmzgtl16@gmail.com>
2025-06-25 16:49:38 +0100

    add getAllAuthors, getAuthorsByNationality and searchAuthors in AuthorService

--- src/main/java/org/example/hbookstore/author/service/AuthorService.java
+++ src/main/java/org/example/hbookstore/author/service/AuthorService.java
@@ -3,10 +3,17 @@ package org.example.hbookstore.author.service;
 import org.example.hbookstore.author.api.dto.AuthorResponse;
 import org.example.hbookstore.author.api.dto.CreateAuthorRequest;
 import org.example.hbookstore.author.api.dto.UpdateAuthorRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AuthorService {
     AuthorResponse createAuthor(CreateAuthorRequest request);
     AuthorResponse getAuthor(Long id);
     AuthorResponse updateAuthor(Long id, UpdateAuthorRequest request);
     void deleteAuthor(Long id) throws Exception;
+
+    Page<AuthorResponse> getAllAuthors(Pageable pageable);
+    Page<AuthorResponse> getAuthorsByNationality(String nationality, Pageable pageable);
+    Page<AuthorResponse> searchAuthors(String query, Pageable pageable);
+
 }
