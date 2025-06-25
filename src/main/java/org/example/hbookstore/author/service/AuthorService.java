34534493caa5dd5ec620079eb58f7547cdc83ebd
hmzgt <hmzgtl16@gmail.com>
2025-06-25 15:34:03 +0100

    implement AuthorService.createAuthor in AuthorServiceImpl

--- src/main/java/org/example/hbookstore/author/service/AuthorService.java
+++ src/main/java/org/example/hbookstore/author/service/AuthorService.java
@@ -1,4 +1,12 @@
 package org.example.hbookstore.author.service;
 
+import org.example.hbookstore.author.api.dto.AuthorResponse;
+import org.example.hbookstore.author.api.dto.CreateAuthorRequest;
+import org.example.hbookstore.author.api.dto.UpdateAuthorRequest;
+
 public interface AuthorService {
+    AuthorResponse createAuthor(CreateAuthorRequest request);
+    AuthorResponse getAuthor(Long id);
+    AuthorResponse updateAuthor(Long id, UpdateAuthorRequest request);
+    void deleteAuthor(Long id);
 }
--- src/main/java/org/example/hbookstore/author/service/AuthorServiceImpl.java
+++ src/main/java/org/example/hbookstore/author/service/AuthorServiceImpl.java
@@ -1,4 +1,49 @@
 package org.example.hbookstore.author.service;
 
+import org.example.hbookstore.author.api.dto.AuthorResponse;
+import org.example.hbookstore.author.api.dto.CreateAuthorRequest;
+import org.example.hbookstore.author.api.dto.UpdateAuthorRequest;
+import org.example.hbookstore.author.domain.Author;
+import org.example.hbookstore.author.domain.AuthorRepository;
+import org.example.hbookstore.author.mapping.AuthorMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final AuthorMapper authorMapper;
+
+    @Autowired
+    public AuthorServiceImpl(
+            AuthorRepository authorRepository,
+            AuthorMapper authorMapper
+    ) {
+        this.authorRepository = authorRepository;
+        this.authorMapper = authorMapper;
+    }
+
+    @Override
+    @Transactional
+    public AuthorResponse createAuthor(CreateAuthorRequest request) {
+        Author author = authorMapper.toEntity(request);
+        return authorMapper.toResponse(authorRepository.save(author));
+    }
+
+    @Override
+    public AuthorResponse getAuthor(Long id) {
+        return null;
+    }
+
+    @Override
+    public AuthorResponse updateAuthor(Long id, UpdateAuthorRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthor(Long id) {
+
+    }
 }
